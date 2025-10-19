9aadbb6dfc555a78b69556aceb5f4142c217df5e
khaled88-kaboub <khaledkaboub88@gmail.com>
2025-10-19 21:54:59 +0100

    Update Api.js
    mise a jour api

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // change selon ton backend
+  baseURL: "https://rest-api-fd2n.onrender.com/api", // change selon ton backend
 });
 
 // ajouter le token automatiquement si présent
